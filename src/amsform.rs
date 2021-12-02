0225f2f15ad88fd8ae23e83b5038750c468c684e
Ensar Sarajčić <es.ensar@gmail.com>
2021-12-02 20:20:04 +0100

    Separate out AMS form filling logic

---
+++ src/amsform.rs
@@ -0,0 +1,208 @@
+extern crate pdf_forms;
+use std::ops::Add;
+
+use pdf_forms::{Form, ValueError};
+
+pub enum FormField {
+    PageNumber = 0,
+    PageCount = 1,
+    UserName = 2,
+    UserJmbg = 3,
+    UserAddress = 4,
+    PaymentDateDay = 5,
+    PaymentDateMonth = 6,
+    PaymentDateYear = 7,
+    TaxPeriodMonth = 8,
+    TaxPeriodYearLast2Digits = 9,
+    CompanyName = 10,
+    CompanyAddress = 11,
+    CompanyCountry = 12,
+    HealthInsuranceTotal = 43,
+    TaxBaseTotal = 44,
+    TaxAmountTotal = 45,
+    TaxPairAbroadTotal = 46,
+    TaxToPayTotal = 47,
+    Date = 48,
+}
+
+enum RepeatingFormField {
+    IncomeValue = 0,
+    HealthInsurance = 1,
+    TaxBase = 2,
+    TaxAmount = 3,
+    TaxPaidAbroad = 4,
+    TaxToPay = 5,
+}
+
+static REPEATING_FIELDS_START: u32 = 13;
+static REPEATED_LINES: u32 = 5;
+static REPEATED_FIELDS_COUNT: u32 = 6;
+
+struct IncomeLine {
+    value: i64,
+    health_insurance: i64,
+    tax_base: i64,
+    tax_amount: i64,
+    tax_paid_abroad: i64,
+    tax_to_pay: i64,
+}
+
+impl Add for IncomeLine {
+    type Output = IncomeLine;
+
+    fn add(self, rhs: IncomeLine) -> Self::Output {
+        IncomeLine {
+            value: self.value + rhs.value,
+            health_insurance: self.health_insurance + rhs.health_insurance,
+            tax_base: self.tax_base + rhs.tax_base,
+            tax_amount: self.tax_amount + rhs.tax_amount,
+            tax_paid_abroad: self.tax_paid_abroad + rhs.tax_paid_abroad,
+            tax_to_pay: self.tax_to_pay + rhs.tax_to_pay,
+        }
+    }
+}
+
+impl Clone for IncomeLine {
+    fn clone_from(&mut self, source: &Self) {
+        *self = source.clone()
+    }
+
+    fn clone(&self) -> Self {
+        Self {
+            value: self.value,
+            health_insurance: self.health_insurance,
+            tax_base: self.tax_base,
+            tax_amount: self.tax_amount,
+            tax_paid_abroad: self.tax_paid_abroad,
+            tax_to_pay: self.tax_to_pay,
+        }
+    }
+}
+impl Copy for IncomeLine {}
+
+pub struct AmsForm {
+    pdf_form: Form,
+    income_lines: Vec<IncomeLine>,
+}
+
+fn format_money_value(value: i64) -> String {
+    format!("{},{:02}", value / 100, value % 100)
+}
+
+fn fill_field(pdf_form: &mut Form, field_index: usize, value: String) {
+    match pdf_form.set_text(field_index, value) {
+        Ok(_) => (),
+        Err(why) => panic!("{:?}", why),
+    }
+}
+
+fn fill_repeating_field(pdf_form: &mut Form, line: u32, field: RepeatingFormField, value: String) {
+    fill_field(
+        pdf_form,
+        (field as u32 + REPEATING_FIELDS_START + line * REPEATED_FIELDS_COUNT)
+            .try_into()
+            .unwrap(),
+        value,
+    );
+}
+
+impl AmsForm {
+    pub fn fill_main_field(&mut self, field: FormField, value: String) {
+        fill_field(&mut self.pdf_form, field as usize, value);
+    }
+
+    pub fn add_income(&mut self, base_value: i64, tax_paid_abroad: i64) {
+        let health_insurance = (base_value * 4) / 100;
+        let tax_base = base_value - health_insurance;
+        let tax_amount = tax_base / 10;
+        let tax_to_pay = tax_amount - tax_paid_abroad;
+        self.income_lines.push(IncomeLine {
+            value: base_value,
+            health_insurance,
+            tax_base,
+            tax_amount,
+            tax_paid_abroad,
+            tax_to_pay,
+        });
+    }
+
+    pub fn save(&mut self, output_file: &str) {
+        // TODO: Handle multiple pages
+        self.fill_main_field(FormField::PageNumber, "1".to_string());
+        self.fill_main_field(FormField::PageCount, "1".to_string());
+        let total = self
+            .income_lines
+            .iter()
+            .copied()
+            .reduce(|acc, x| acc + x)
+            .unwrap();
+        let mut counter = 0;
+        for income_line in &self.income_lines {
+            fill_repeating_field(
+                &mut self.pdf_form,
+                counter,
+                RepeatingFormField::IncomeValue,
+                format_money_value(income_line.value),
+            );
+            fill_repeating_field(
+                &mut self.pdf_form,
+                counter,
+                RepeatingFormField::HealthInsurance,
+                format_money_value(income_line.health_insurance),
+            );
+            fill_repeating_field(
+                &mut self.pdf_form,
+                counter,
+                RepeatingFormField::TaxBase,
+                format_money_value(income_line.tax_base),
+            );
+            fill_repeating_field(
+                &mut self.pdf_form,
+                counter,
+                RepeatingFormField::TaxAmount,
+                format_money_value(income_line.tax_amount),
+            );
+            fill_repeating_field(
+                &mut self.pdf_form,
+                counter,
+                RepeatingFormField::TaxPaidAbroad,
+                format_money_value(income_line.tax_paid_abroad),
+            );
+            fill_repeating_field(
+                &mut self.pdf_form,
+                counter,
+                RepeatingFormField::TaxToPay,
+                format_money_value(income_line.tax_to_pay),
+            );
+            counter += 1;
+        }
+        self.fill_main_field(
+            FormField::HealthInsuranceTotal,
+            format_money_value(total.health_insurance),
+        );
+        self.fill_main_field(FormField::TaxBaseTotal, format_money_value(total.tax_base));
+        self.fill_main_field(
+            FormField::TaxAmountTotal,
+            format_money_value(total.tax_amount),
+        );
+        self.fill_main_field(
+            FormField::TaxPairAbroadTotal,
+            format_money_value(total.tax_paid_abroad),
+        );
+        self.fill_main_field(
+            FormField::TaxToPayTotal,
+            format_money_value(total.tax_to_pay),
+        );
+        match self.pdf_form.save(output_file) {
+            Ok(_) => (),
+            Err(why) => panic!("{:?}", why),
+        }
+    }
+}
+
+pub fn load_ams_form(input_file: &str) -> AmsForm {
+    AmsForm {
+        pdf_form: Form::load(input_file).unwrap(),
+        income_lines: Vec::new(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,11 @@
-extern crate pdf_forms;
-use pdf_forms::Form;
-
-use std::collections::HashMap;
+mod amsform;
+use amsform::FormField;
 
 fn main() {
-    let form_fields = HashMap::from([
-        ("page_number", 0),
-        ("page_count", 1),
-        ("user_name", 2),
-        ("user_jmbg", 3),
-        ("user_address", 4),
-        ("payment_date_day", 5),
-        ("payment_date_month", 6),
-        ("payment_date_year", 7),
-        ("tax_period_month", 8),
-        ("tax_period_year_last2", 9),
-        ("company_name", 10),
-        ("company_address", 11),
-        ("company_country", 12),
-        ("health_insurance_total", 43),
-        ("tax_base_total", 44),
-        ("tax_amount_total", 45),
-        ("tax_paid_abroad_total", 46),
-        ("tax_to_pay_total", 47),
-        ("date", 48),
-    ]);
+    let mut form = amsform::load_ams_form("tax.pdf");
 
-    let repeating_fields = HashMap::from([
-        ("income_value", 0),
-        ("health_insurance", 1),
-        ("tax_base", 2),
-        ("tax_amount", 3),
-        ("tax_paid_abroad", 4),
-        ("tax_to_pay", 5),
-    ]);
-    let repeating_fields_start = 13;
-    let repeated_lines = 5;
+    form.fill_main_field(FormField::UserName, "Ensar".to_string());
+    form.add_income(100000, 0);
 
-    let mut form = Form::load("tax.pdf").unwrap();
-
-    for (key, index) in form_fields {
-        match form.set_text(index, format!("{}", key)) {
-            Ok(res) => println!("Field {} set! Result: {:?}", index, res),
-            Err(why) => panic!("{:?}", why),
-        }
-    }
-    for i in 0..repeated_lines {
-        repeating_fields.iter().for_each(|(key, index)| {
-            match form.set_text(
-                repeating_fields_start + index + i * repeating_fields.len(),
-                format!("{} line {}", key, i),
-            ) {
-                Ok(res) => println!("Field {} set! Result: {:?}", index, res),
-                Err(why) => panic!("{:?}", why),
-            }
-        })
-    }
-    let result = form.save("taxresult.pdf");
-    match result {
-        Ok(res) => println!("saving success: {:?}", res),
-        Err(why) => panic!("{:?}", why),
-    }
+    form.save("taxresult.pdf");
 }
