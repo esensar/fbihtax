0881efee449b3a2edb75cdcd8ec2cd29333b1eae
Ensar Sarajčić <es.ensar@gmail.com>
2021-12-09 12:49:09 +0100

    Add basic CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
+extern crate clap;
 mod amsform;
 mod config;
+use clap::Parser;
 use std::path::Path;
 
 use amsform::FormField;
 
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct CliArgs {
+    #[clap(short, long, default_value = ".fbihtax.json")]
+    config: String,
+    #[clap(short, long)]
+    income: f64,
+}
+
 fn main() {
-    let config = config::parse_config(".fbihtax.json");
+    let args = CliArgs::parse();
+
+    let config = config::parse_config(args.config.as_str());
     let mut form = amsform::load_ams_form(config.pdf.cache_location);
 
     form.fill_main_field(FormField::UserName, "Ensar".to_string());
-    form.add_income(100000, 0);
+    let income_rounded = (args.income * 100.0).round() as i64;
+    form.add_income(income_rounded, 0);
 
     form.save(
         Path::new(config.output_location.as_str())
